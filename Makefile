3402c151c1fbba84a7d8e342161b52a7169224fd
l0py2 <l0py2.git@gmail.com>
2024-05-24 20:13:31 +0100

    Table template added

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRC := main.c
 OBJ := ${SRC:.c=.o}
 OUT := shiro.cgi
+TEMPLATES := templates
 INSTALL_OUT := /srv/http/cgi-bin
 CFLAGS := -Wall
 LDLIBS := `pkg-config --libs sqlite3`
@@ -12,9 +13,10 @@ clean:
 
 install:
 	@cp -f ${OUT} ${INSTALL_OUT}
+	@cp -rfT ${TEMPLATES} ${INSTALL_OUT}/${TEMPLATES}
 
 uninstall:
-	@rm -f ${INSTALL_OUT}
+	@rm -rf ${INSTALL_OUT}/${OUT} ${INSTALL_OUT}/${TEMPLATES}
 
 ${OBJ}:
 
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
+#include <string.h>
 
 #define DATABASE_PATH "/srv/anime.db"
+#define TABLE_TEMPLATE "templates/table.html"
 
-int main(int argc, char** argv) {
+void print_table(sqlite3* database);
+
+int main(void) {
 	sqlite3* database;
 	int result;
 
@@ -20,8 +24,40 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	FILE* template = fopen(TABLE_TEMPLATE, "r");
+
+	if(!template) {
+		printf("Status: 500\n");
+		printf("Content-Type: text/plain\n\n");
+		printf("Template error\n");
+
+		return 1;
+	}
+
 	printf("Content-Type: text/html\n\n");
 
+	const int BUFF_SIZE = 1024;
+	char buff[BUFF_SIZE];
+	const char* tag = "<c/>";
+
+	while(fgets(buff, BUFF_SIZE, template)) {
+		if(!strstr(buff, tag)) {
+			printf(buff);
+
+			continue;
+		}
+
+		print_table(database);
+	}
+
+	fclose(template);
+	sqlite3_close(database);
+
+	return 0;
+}
+
+void print_table(sqlite3* database) {
+	int result;
 	sqlite3_stmt* query;
 
 	result = sqlite3_prepare_v2(database, "SELECT id, name, rating FROM anime", -1, &query, NULL);
@@ -30,24 +66,28 @@ int main(int argc, char** argv) {
 		sqlite3_finalize(query);
 		sqlite3_close(database);
 
-		return 1;
+		return;
 	}
 
-	printf("<ul>");
+	printf(
+		"<table>"
+		"<tr>"
+		"<th>ID</th>"
+		"<th>Name</th>"
+		"<th>Rating</th>"
+		"</tr>"
+	);
 
 	while(sqlite3_step(query) == SQLITE_ROW) {
 		printf(
-			"<li>[%d] %s - %d</li>",
+			"<tr><td>%d</td><td>%s</td><td>%d</td></tr>",
 			sqlite3_column_int(query, 0),
 			sqlite3_column_text(query, 1),
 			sqlite3_column_int(query, 2)
 		);
 	}
 
-	printf("</ul>");
+	printf("</table>");
 
 	sqlite3_finalize(query);
-	sqlite3_close(database);
-
-	return 0;
 }
